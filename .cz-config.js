c6e2fab82bc09477fa1c9ddb02e073e64ef24fcd
staven <20287710@qq.com>
2019-06-12 00:22:08 +0800

    :heavy_plus_sign: dep_add(修炼React): react生命周期
    新增"React生命周期"、"git commit message规范化提交"
    修改"React Hooks"
    增加"前端工程化——Git"

--- .cz-config.js
+++ .cz-config.js
@@ -2,28 +2,20 @@ module.exports = {
   // types,
   types: [
     {
-      value: ':sparkles: feat',
-      name: '✨ 增加新功能'
+      value: ':heavy_plus_sign: dep_add',
+      name: '➕ 添加文章'
     },
     {
-      value: ':bug: fix',
-      name: '🐛 修复bug'
+      value: ':heavy_minus_sign: dep_rm',
+      name: '➖ 删除文章'
     },
     {
       value: ':pencil: docs',
-      name: '📝 修改文档(仅文档更改)'
-    },
-    {
-      value: ':lipstick: ui',
-      name: '💄 更新UI(更新用户界面和样式文件 )'
+      name: '📝 修改文章'
     },
     {
       value: ':art: style',
-      name: '🎨 改进代码的结构（空白、格式、缺少分号等）'
-    },
-    {
-      value: ':recycle: refactor',
-      name: '♻️  重构代码'
+      name: '🎨 修改目录'
     },
     {
       value: ':bookmark: release',
@@ -31,40 +23,15 @@ module.exports = {
     },
     {
       value: ':rocket: deploy',
-      name: '🚀 部署'
-    },
-    {
-      value: ':white_check_mark: test',
-      name: '✅ 增删测试'
-    },
-    {
-      value: ':wrench: chore',
-      name: '🔧 更改配置(构建过程或辅助工具的变动，包配置文件)'
+      name: '🚀 发布文章'
     },
     {
       value: ':tada: init',
       name: '🎉 初次提交'
-    },
-    {
-      value: ':heavy_plus_sign: dep_add',
-      name: '➕ 添加依赖'
-    },
-    {
-      value: ':heavy_minus_sign: dep_rm',
-      name: '➖ 删除依赖'
-    },
-    {
-      value: ':whale: docker',
-      name: '🐳 Docker'
     }
   ],
 
-  scopes: [
-    { name: 'common' },
-    { name: 'build' },
-    { name: 'core' },
-    { name: 'upgrade' }
-  ],
+  scopes: [{ name: '修炼React' }, { name: '前端工程化' }],
 
   // it needs to match the value for field type. Eg.: 'fix'
 
